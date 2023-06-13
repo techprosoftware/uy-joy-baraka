c0dd84a4a37b53c3b423165f68375358c1f73132
husanovjamshid <husanovjamshid11@gmail.com>
2023-06-13 09:36:38 -0700

    change app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import ReactDOM from "react-dom/client";
-import { Provider } from 'react-redux'
-import App from "./App.jsx";
-import "./main.css";
-import store from "./redux/store.js";
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App.jsx';
+import './main.scss';
+import store from './redux/store.js';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
 );
