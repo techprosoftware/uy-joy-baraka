7ac70ba564a05aff72959c7ce1dd0799e011406f
Bunyodbek <93827769+BunyodbekNasriddinov@users.noreply.github.com>
2023-06-13 09:38:19 -0700

    site logo add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,22 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import './header.scss'
+import { Link } from 'react-router-dom'
+import SiteLogo from '@images/logo.svg'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div className='site-header'>
+      <div className="container">
+        <div className="site-haeder__inner">
+          <Link to='/'><img src={SiteLogo} width={133} height={59} alt="Site logo" /></Link>
+        </div>
+        <nav>
+          <ul>
+
+          </ul>
+        </nav>
+      </div>
+    </div>
   )
 }
