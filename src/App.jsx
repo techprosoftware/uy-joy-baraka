c955ed70d3b57434af1d0b0677ea4fa7e3e3b2db
Jamshidbek Husanov <94694648+husanovjamshid@users.noreply.github.com>
2023-07-03 16:50:52 +0500

    change upload

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,11 @@ import { Register } from './pages/Register/Register';
 import { Login } from './pages/Login/Login';
 import { SmsPage } from './pages/smsPage/SmsPage';
 import { CardSingle } from './components/CardSingle/CardSingle'
-<<<<<<< HEAD
 import { NotFoundError } from './pages/404/404';
-=======
 import { Messaging } from './pages/Messaging/Messaging'
->>>>>>> 3275e23feb884e78e787928942460ddfbb9ceee3
-
 
 function App() {
 	return (
-<<<<<<< HEAD
-		<>
-	
-			<Routes>
-				<Route path='/' element={<Public />} />
-				<Route path='/register' element={<Register/>}/>
-				<Route path='/login' element={<Login/>}/>
-				<Route path='/sms' element={<SmsPage/>}/>
-				<Route path='/card' element={<CardSingle/>}/>
-
-			</Routes>
-		
-			
-		</>
-	);
-=======
     <>
       <Routes>
         <Route path="/*" element={<Public />} />
@@ -56,7 +36,6 @@ function App() {
       </Routes> */}
     </>
   );
->>>>>>> 3275e23feb884e78e787928942460ddfbb9ceee3
 }
 
 export default App;
