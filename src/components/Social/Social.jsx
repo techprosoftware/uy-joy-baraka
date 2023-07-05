59b1151a2190a3700a22382f595615cd12ec24fc
Bunyodbek <93827769+BunyodbekNasriddinov@users.noreply.github.com>
2023-07-05 17:59:08 +0500

    add: social component & fix: ads style

---
+++ src/components/Social/Social.jsx
@@ -0,0 +1,19 @@
+import LogoSocial from "@images/logo-social.svg"
+
+export const Social = () => {
+  return (
+    <div className="social">
+      <div className="container">
+        <img
+          className=""
+          src={LogoSocial}
+          width={190}
+          height={85}
+          alt="Site logo"
+        />
+        <h2 className="">Bizni ijtimoiy tarmoqlardakuzating</h2>
+        <div className=""></div>
+      </div>
+    </div>
+  )
+}
