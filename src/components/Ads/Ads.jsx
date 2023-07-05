a4dd579de65fe53bb4546bc53ca2f23627ca0dcd
Bunyodbek <93827769+BunyodbekNasriddinov@users.noreply.github.com>
2023-07-05 16:40:32 +0500

    add: ads component

--- src/components/Ads/Ads.jsx
+++ src/components/Ads/Ads.jsx
@@ -1,8 +1,29 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react"
+import ButtonPhoneIcon from "@images/home-phone.svg"
+import AdsBgi from "@images/ads-bgi.png"
+import "./ads.scss"
 
 export const Ads = () => {
   return (
-    <div>Ads</div>
+    <div className="ads">
+      <div className="container">
+        <div className="ads__title">
+          <h2>Reklamangiz uchun joy</h2>
+        </div>
+        <button
+          className="banner-btn ads__btn"
+          href="#"
+        >
+          <img
+            width={25}
+            height={25}
+            src={ButtonPhoneIcon}
+            alt="buton-phone"
+          />
+          {"Biz bilan bog’lanish"}
+        </button>
+      </div>
+    </div>
   )
 }
--- src/pages/Home/HomeCards/HomeCards.jsx
+++ src/pages/Home/HomeCards/HomeCards.jsx
@@ -3,8 +3,6 @@
 import React from 'react'
 import './HomeCard.scss'
 import { CardList } from '@components/CardList/CardList'
-import { MoreBtn } from '../../../components/MoreBtn/MoreBtn'
-
 export const HomeCards = () => {
 
   return (
@@ -12,7 +10,6 @@ export const HomeCards = () => {
     <div className="home__card__inner">
       <div className="container">
         <CardList/>
-        <MoreBtn/>
       </div>
     </div>
     </>
