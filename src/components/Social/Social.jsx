6cfc3e556c09e7174952a71e533208f9973fa450
Bunyodbek <93827769+BunyodbekNasriddinov@users.noreply.github.com>
2023-07-07 18:20:22 +0500

    change social page

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -1,18 +1,70 @@
 import LogoSocial from "@images/logo-social.svg"
+import "./social.scss"
+import { Link } from "react-router-dom"
+import FacebookIcon from "@images/social-facebook-icon.svg"
+import InstagramIcon from "@images/social-instagram-icon.svg"
+import TelegramIcon from "@images/social-telegram-icon.svg"
 
 export const Social = () => {
   return (
     <div className="social">
       <div className="container">
-        <img
-          className=""
-          src={LogoSocial}
-          width={190}
-          height={85}
-          alt="Site logo"
-        />
-        <h2 className="">Bizni ijtimoiy tarmoqlardakuzating</h2>
-        <div className=""></div>
+        <div className="social__inner">
+          <img
+            className="social__logo"
+            src={LogoSocial}
+            width={190}
+            height={85}
+            alt="Site logo"
+          />
+          <div className="social__title">
+            <h2>Bizni ijtimoiy tarmoqlarda kuzating</h2>
+          </div>
+          <ul className="social__list">
+            <li className="social__item">
+              <Link
+                className="social__link"
+                to={`#`}
+              >
+                <img
+                  className="social__icon"
+                  src={FacebookIcon}
+                  width={35}
+                  height={35}
+                  alt="Facebook icon"
+                />
+              </Link>
+            </li>
+            <li className="social__item">
+              <Link
+                className="social__link"
+                to={`#`}
+              >
+                <img
+                  className="social__icon"
+                  src={InstagramIcon}
+                  width={35}
+                  height={35}
+                  alt="Facebook icon"
+                />
+              </Link>
+            </li>
+            <li className="social__item">
+              <Link
+                className="social__link"
+                to={`#`}
+              >
+                <img
+                  className="social__icon"
+                  src={TelegramIcon}
+                  width={35}
+                  height={35}
+                  alt="Facebook icon"
+                />
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   )
