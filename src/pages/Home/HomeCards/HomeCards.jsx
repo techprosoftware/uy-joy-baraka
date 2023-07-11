8487f3486cb5d893f0bc9c7a0205a154c1310949
Jamshidbek Husanov <94694648+husanovjamshid@users.noreply.github.com>
2023-07-11 11:25:55 +0500

    fix: homecards page

--- src/pages/Home/HomeCards/HomeCards.jsx
+++ src/pages/Home/HomeCards/HomeCards.jsx
@@ -1,18 +1,17 @@
 /* eslint-disable no-unused-vars */
 
-import React from 'react'
-import './HomeCard.scss'
-import { CardList } from '@components/CardList/CardList'
+import React from "react";
+import "./HomeCard.scss";
+import { CardList } from "@components/CardList/CardList";
 export const HomeCards = () => {
-
   return (
     <>
-    <div className="home__card-inner">
-      <div className="container">
-       <h2 className="home__card-title">Sizga mos keladigan uylar</h2>
-        <CardList/>
+      <div className="home__card-inner">
+        <div className="container">
+          <h2 className="home__card-title">Sizga mos keladigan uylar</h2>
+          <CardList />
+        </div>
       </div>
-    </div>
     </>
   );
 };
