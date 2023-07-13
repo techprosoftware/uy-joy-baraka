f56a04761433fe7bf4a5b1dc2f9348dc38bc5f49
Bunyodbek <93827769+BunyodbekNasriddinov@users.noreply.github.com>
2023-07-13 12:38:51 +0500

    finally: card-single responsive

--- src/components/CardSingle/CardSingle.jsx
+++ src/components/CardSingle/CardSingle.jsx
@@ -7,10 +7,8 @@ import data from "./data"
 import { MoreBtn } from "../MoreBtn/MoreBtn"
 import { useState } from "react"
 import { motion } from "framer-motion"
-import Skeleton from "react-loading-skeleton"
 
 export const CardSingle = () => {
-  let [isLoading, setIsLoading] = useState(false)
   let [imgId, setImgId] = useState(1)
 
   return (
@@ -34,103 +32,56 @@ export const CardSingle = () => {
             >
               <div className="card-single__pics">
                 <div className="img-select">
-                  {data.pics.map((img, idx) =>
-                    isLoading ? (
-                      <Skeleton
-                      className="mb-2"
-                        key={idx}
-                        width={120}
-                        height={120}
-                      />
-                    ) : (
-                      <img
-                        onClick={() => setImgId(idx)}
-                        className={`img-item card-single__img-${idx}`}
-                        key={img.id}
-                        src={img.src}
-                        width={120}
-                        height={120}
-                        data-id={idx}
-                      />
-                    )
-                  )}
+                  {data.pics.map((img, idx) => (
+                    <img
+                      onClick={() => setImgId(idx)}
+                      className={`img-item card-single__img-${idx}`}
+                      key={img.id}
+                      src={img.src}
+                      width={120}
+                      height={120}
+                      data-id={idx}
+                    />
+                  ))}
                 </div>
 
                 <div className="img-display">
                   <div className="img-showcase">
-                    {isLoading ? (
-                      <Skeleton
-                        width={466}
-                        height={525}
-                      />
-                    ) : (
-                      <img
-                        className="show-image"
-                        width={466}
-                        height={525}
-                        src={data.pics[imgId].src}
-                        alt="Home images"
-                        aria-label="Home images"
-                      />
-                    )}
+                    <img
+                      className="show-image"
+                      width={466}
+                      height={525}
+                      src={data.pics[imgId].src}
+                      alt="Home images"
+                      aria-label="Home images"
+                    />
                   </div>
                 </div>
               </div>
               <div className="card-single__content">
                 <div className="card-single__top">
                   <div className="d-flex card-single__date">
-                    <time className="card-single__time">
-                      {isLoading ? <Skeleton width={71} /> : data.created_at}
-                    </time>
-                    <span className="card-single__view">
-                      {isLoading ? <Skeleton width={21} /> : data.view}
-                    </span>
+                    <time className="card-single__time">{data.created_at}</time>
+                    <span className="card-single__view">{data.view}</span>
                   </div>
                   <button className="card-single__share-btn">Ulashish</button>
                   <div className="card-single__user-info">
-                    {isLoading ? (
-                      <Skeleton
-                        width={109}
-                        height={32}
-                      />
-                    ) : (
-                      <>
-                        <img
-                          className="card-single__user-avatar"
-                          src={data.user.avatar}
-                          width={32}
-                          height={32}
-                          alt={data.user.username}
-                        />
-                        <span className="card-single__user-name">
-                          {`${data.user.username.split(" ")[0]}\n${
-                            data.user.username.split(" ")[1]
-                          }`}
-                        </span>
-                      </>
-                    )}
+                    <img
+                      className="card-single__user-avatar"
+                      src={data.user.avatar}
+                      width={32}
+                      height={32}
+                      alt={data.user.username}
+                    />
+                    <span className="card-single__user-name">
+                      {`${data.user.username.split(" ")[0]}\n${
+                        data.user.username.split(" ")[1]
+                      }`}
+                    </span>
                   </div>
                 </div>
-                <h2 className="card-single__title">
-                  {isLoading ? (
-                    <Skeleton
-                      width={463}
-                      height={56}
-                    />
-                  ) : (
-                    data.title
-                  )}
-                </h2>
-                <p className="card-single__text">
-                  {isLoading ? (
-                    <Skeleton
-                      width={463}
-                      height={136}
-                    />
-                  ) : (
-                    data.body
-                  )}
-                </p>
+                <h2 className="card-single__title">{data.title}</h2>
+                <p className="card-single__text">{data.body}</p>
                 <form method="POST">
                   <textarea
                     className="card-single__area"
