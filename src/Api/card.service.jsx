309269c48221214a84c274fb46aa69af6805f479
Bunyodbek <93827769+BunyodbekNasriddinov@users.noreply.github.com>
2023-07-17 20:17:32 +0500

    card.service add

--- src/Api/card.service.jsx
+++ src/Api/card.service.jsx
@@ -2,7 +2,7 @@ import axios from "./api";
 
 const CardService = {
   getAllCard: async () => await axios.get(`/api/home`),
-  getByCard: async (slug) => await axios.get(`/api/home/${slug}`),
+  getByCard: async (slug) => await axios.get(`/api/posts/${slug}`),
 }
 
 export default CardService;
