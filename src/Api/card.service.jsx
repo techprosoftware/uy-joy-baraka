934a4f098a83a78b5bf2babf061c7feb7b2ed357
husanovjamshid <husanovjamshid11@gmail.com>
2023-07-27 16:24:05 +0500

    add like page

--- src/Api/card.service.jsx
+++ src/Api/card.service.jsx
@@ -1,6 +1,6 @@
 import axios from "./api";
 
-const token = localStorage.getItem('token')
+const token = localStorage.getItem("token");
 
 const CardService = {
   getAllCard: async () => await axios.get(`/api/home`),
@@ -8,23 +8,36 @@ const CardService = {
   getByPage: async (page) => await axios.get(`/api/home?c_page=${page}`),
   likeCard: async (id) => {
     try {
-      return await axios.patch(`/api/announcements/${id}/like`, {announcement_id: id}, {
+      return await axios.patch(
+        `/api/announcements/${id}/like`,
+        { announcement_id: id },
+        {
+          headers: { authorization: token },
+        }
+      );
+    } catch (err) {
+      return await err;
+    }
+  },
+  unLikeCard: async (id) => {
+    try {
+      return await axios.patch(`/api/announcements/${id}/unlike`, {}, {
         headers: {authorization: token},
       })
     } catch (err) {
       return await err
     }
-  }
-  ,
+  },
   getLike: async () => {
     try {
       return await axios.get(`/api/announcements/liked`, {
-        headers: {authorization: token},
-      })
+        headers: { authorization: token },
+      });
     } catch (err) {
-      return await err
+      return await err;
     }
-  }
-}
+  },
+};
 
 export default CardService;
+// api/announcements/f26b2f68-5cd0-4fd9-875c-41163c90e0b3/unlike
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import {useEffect, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {setCard} from "@/redux/card/cardAction.js";
 import CardService from "@/Api/card.service.jsx";
@@ -8,36 +8,51 @@ import {Card} from "../Cards/Cards";
 import {CardSkeleton} from "@components/Cards/CardSkeleton";
 import "./card-list.scss";
 
+
 export const CardList = ({page, count, end}) => {
   const [isLoading, setIsLoading] = useState(true);
   const dispatch = useDispatch();
   const mockData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const [data, setData] = useState([]);
   // const cardSelector = useSelector((state) => state.cards.card);
-  async function fetchCardData() {
-    try {
-      const response = await CardService.getByPage(page);
-      if (response.status === 200) {
-        setIsLoading(false);
-        const data = response.data.posts;
-        setData(data);
-        // dispatch(setCard(data));
+  
+
+  // const result = useMemo(() => {
+  //   async function fetchCardData() {
+  //     try {
+  //       const response = await CardService.getByPage(page);
+  //       console.log(response);
+  //       if (response.status === 200) {
+  //         setIsLoading(false);
+  //         const data = response.data.posts;
+  //         setData(data);
+  //         // dispatch(setCard(data));
+  //       }
+  //     } catch (error) {
+  //       console.error("Error fetching card data:", error);
+  //     }
+  //   }
+
+  //   fetchCardData()
+  // },[])
+  
+  useEffect(() => { 
+      async function fetchCardData() {
+        try {
+          const response = await CardService.getByPage(page);
+          console.log(response);
+          if (response.status === 200) {
+            setIsLoading(false);
+            const data = response.data.posts;
+            setData(data);
+            // dispatch(setCard(data));
+          }
+        } catch (error) {
+          console.error("Error fetching card data:", error);
+        }
       }
-    } catch (error) {
-      console.error("Error fetching card data:", error);
-    }
-  }
-
-  useEffect(() => {
-    // if (cardSelector.length) {
-    //   setIsLoading(false);
-    //   setData(cardSelector)
-    // } else {
-    // fetchCardData();
-    // }
-      fetchCardData();
-
-  }, [data]);
+  
+      fetchCardData()}, [])
 
   return (
     <ul className="card-list">
@@ -48,6 +63,7 @@ export const CardList = ({page, count, end}) => {
         )) : data?.slice(0, count)?.map((card) => (
           <Card key={card.announcement_id} card={card}/>
         ))}
+        
     </ul>
   );
 };
