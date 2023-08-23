3a9ef63ef9b1b6087206611cd41f32bae7a6cd57
husanovjamshid <husanovjamshid11@gmail.com>
2023-08-23 13:19:43 +0500

    change header page

--- src/components/HomeSearch/HomeSearch.jsx
+++ src/components/HomeSearch/HomeSearch.jsx
@@ -262,8 +262,8 @@ const HomeSearch = () => {
               <Link to={"announsearch"} onClick={handleSubmitSearch} href="#">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
+                  width="22"
+                  height="22"
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="#fff"
